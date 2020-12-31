c85f61e840be99b76e1cc78dd80fa3c4df8e8441
bythealphabet <bythealphabet@gmail.com>
2020-12-31 05:53:29 -0400

    adding hmr to webpack dev file

--- build-utils/webpack.config.client.development.js
+++ build-utils/webpack.config.client.development.js
@@ -6,7 +6,7 @@ const CURRENT_WORKING_DIR = process.cwd();
 function webpackClientDevelopment(mode, name) {
   return {
     name,
-    devtool: "cheap-module-source-map",
+    devtool: "eval-source-map",
     entry: [
       "react-hot-loader/patch",
       "webpack-hot-middleware/client?reload=true",
